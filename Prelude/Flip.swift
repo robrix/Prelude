f6f3f8c3a4e74b2f44eb4db8d77bbc744285359a
Rob Rix <rob.rix@github.com>
2014-12-07 13:43:34 -0500

    Document `flip`.

--- Prelude/Flip.swift
+++ Prelude/Flip.swift
@@ -1,5 +1,8 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+/// Returns a binary function which calls `f` with its arguments reversed.
+///
+/// I.e. `flip(f)(x, y)` is equivalent to `f(y, x)`
 public func flip<T, U, V>(f: (T, U) -> V) -> (U, T) -> V {
 	return { f($1, $0) }
 }
