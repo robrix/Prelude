f1f6fb901fcf14124a12a4ed7cc1f5abc5c6d486
Rob Rix <rob.rix@github.com>
2015-02-17 22:50:21 -0500

    Use @autoclosure for the right operand.

--- Prelude/Conjunction.swift
+++ Prelude/Conjunction.swift
@@ -3,8 +3,11 @@
 // MARK: - Optional conjunction
 
 /// Returns a tuple of two `Optional` values, or `nil` if either or both are `nil`.
-public func &&& <T, U> (left: T?, right: U?) -> (T, U)? {
-	return left.map { x in right.map { y in (x, y) } } ?? nil
+public func &&& <T, U> (left: T?, @autoclosure right: () -> U?) -> (T, U)? {
+	if let x = left, let y = right() {
+		return (x, y)
+	}
+	return nil
 }
 
 
