9d0564298f9f63ac919ebb307f97358d9cf3664c
Rob Rix <rob.rix@github.com>
2015-02-17 22:32:28 -0500

    Documentation.

--- Prelude/Conjunction.swift
+++ Prelude/Conjunction.swift
@@ -2,6 +2,7 @@
 
 // MARK: - Optional conjunction
 
+/// Returns a tuple of two `Optional` values, or `nil` if either or both are `nil`.
 public func &&& <T, U> (left: T?, right: U?) -> (T, U)? {
 	return left.map { x in right.map { y in (x, y) } } ?? nil
 }
