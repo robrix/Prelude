fe74b9eec1fb38862e8a7f00e62031c64c13b021
Rob Rix <rob.rix@github.com>
2015-02-17 22:29:41 -0500

    Implement conjunction over `Optional`s.

--- Prelude/Conjunction.swift
+++ Prelude/Conjunction.swift
@@ -1,5 +1,12 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
+// MARK: - Optional conjunction
+
+public func &&& <T, U> (left: T?, right: U?) -> (T, U)? {
+	return left.map { x in right.map { y in (x, y) } } ?? nil
+}
+
+
 // MARK: - Operators
 
 infix operator &&& {
